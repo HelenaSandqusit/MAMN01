70adc0e6beeba3f9a9ece1659f84e9fbd699fc31
Helena Sandquist <dic15hsa@student.lu.se>
2020-04-02 21:19:34 +0200

    started with compass

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.myfirstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user taps the Compass button */
+    public void startCompass(View view) {
+        Intent intent = new Intent(this, DisplayCompassActivity.class);
+        startActivity(intent);
+
     }
 }
