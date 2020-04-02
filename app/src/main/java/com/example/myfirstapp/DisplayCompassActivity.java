a5a4f2baf4160cec2e818f5bb6478de333ad6763
Helena Sandquist <dic15hsa@student.lu.se>
2020-04-02 21:25:27 +0200

    started with compass

--- app/src/main/java/com/example/myfirstapp/DisplayCompassActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayCompassActivity.java
@@ -2,27 +2,15 @@ package com.example.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
 
 
-public class DisplayCompassActivity extends AppCompatActivity implements SensorEventListener {
+public class DisplayCompassActivity extends AppCompatActivity  {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_compass);
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
 
